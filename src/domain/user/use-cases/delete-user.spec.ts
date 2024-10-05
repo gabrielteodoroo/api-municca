03904b58a3e0793a37f2a3a03c7911f0fa2cbb30
Teodoro <biel.teodorob@gmail.com>
2024-10-05 12:18:37 -0300

    test: delete user use case

---
+++ src/domain/user/use-cases/delete-user.spec.ts
@@ -0,0 +1,39 @@
+import { InMemoryUserRepository } from '../../../../test/repositories/in-memory-user-repository';
+import Identity from '../../../core/entities/identity';
+import Email from '../../shared/email';
+import User from '../entities/user';
+import { DeleteUserUseCase } from './delete-user';
+
+let userRepository: InMemoryUserRepository;
+let useCase: DeleteUserUseCase;
+
+describe('Delete user', () => {
+  beforeEach(() => {
+    userRepository = new InMemoryUserRepository();
+    useCase = new DeleteUserUseCase(userRepository);
+
+    const user = User.create(
+      {
+        name: 'Gabriel Teodoro',
+        email: Email.create('gabriel@email.com'),
+      },
+      new Identity('1'),
+    );
+
+    userRepository.items.push(user);
+  });
+
+  test('should delete a user', async () => {
+    expect(userRepository.items).toHaveLength(1);
+    const response = await useCase.handle({ id: '1' });
+
+    expect(response.isRight()).toBe(true);
+    expect(userRepository.items).toHaveLength(0);
+  });
+
+  test('should returns null if invalid id is provided', async () => {
+    const response = await useCase.handle({ id: '2' });
+
+    expect(response.isLeft()).toBe(true);
+  });
+});
