0d431abfad1c0eacbe535b52a7f25138d07f47cc
Teodoro <biel.teodorob@gmail.com>
2024-10-05 10:55:30 -0300

    feat: User repository and in memory user repository

---
+++ src/domain/user/repositories/user-repository.ts
@@ -0,0 +1,9 @@
+import User from '../entities/user';
+
+export abstract class UserRepository {
+  abstract create(user: User): Promise<User>;
+  abstract findById(id: string): Promise<User | null>;
+  abstract findByEmail(email: string): Promise<User | null>;
+  abstract save(user: User): Promise<void>;
+  abstract delete(id: string): Promise<void>;
+}
