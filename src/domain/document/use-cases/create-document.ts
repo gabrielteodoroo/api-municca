a77053041e677e975dce94874dd77c489aec2597
Teodoro <biel.teodorob@gmail.com>
2024-10-05 16:49:58 -0300

    feat: Create document use case

---
+++ src/domain/document/use-cases/create-document.ts
@@ -0,0 +1,38 @@
+import { Either, left, right } from '@/core/errors/either/either';
+import { DocumentRepository } from '../repositories/document-repository';
+import Document from '../entities/document';
+import { UserRepository } from '@/domain/user/repositories/user-repository';
+import { NotFoundError } from '@/core/errors/custom-errors/not-found-error';
+
+type Request = {
+  userId: string;
+  name: string;
+  status: string;
+};
+
+type Response = Either<NotFoundError, Document>;
+
+export class CreateDocumentUseCase {
+  constructor(
+    private documentRepository: DocumentRepository,
+    private userRepository: UserRepository,
+  ) {}
+
+  async handle({ name, status, userId }: Request): Promise<Response> {
+    const userExists = await this.userRepository.findById(userId);
+
+    if (!userExists) {
+      return left(new NotFoundError());
+    }
+
+    const document = Document.create({
+      name,
+      status,
+      user: userExists,
+    });
+
+    await this.documentRepository.create(document);
+
+    return right(document);
+  }
+}
