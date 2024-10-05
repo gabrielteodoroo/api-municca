aef5fa678a8592aff4bbd8a65009fcf57dc207b7
Teodoro <biel.teodorob@gmail.com>
2024-10-05 19:30:46 -0300

    feat: add env validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { validate } from './env.validation';
+import { HttpModule } from './infra/http/http.module';
+import { AuthModule } from './infra/auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validate,
     }),
+    HttpModule,
+    AuthModule,
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
