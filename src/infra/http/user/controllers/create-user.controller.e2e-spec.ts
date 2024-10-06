9f7e6c632e332b472ecc76a2f2d1de706fe5858e
Teodoro <biel.teodorob@gmail.com>
2024-10-05 21:10:45 -0300

    chore: Required changes for the create controller test to work with authentication

--- src/infra/http/user/controllers/create-user.controller.e2e-spec.ts
+++ src/infra/http/user/controllers/create-user.controller.e2e-spec.ts
@@ -2,12 +2,17 @@ import { AppModule } from '@/app.module';
 import { DatabaseModule } from '@/infra/database/database.module';
 import { PrismaService } from '@/infra/database/prisma/prisma.service';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { randomUUID } from 'crypto';
 import * as request from 'supertest';
 
 describe('Create user controller', () => {
   let app: INestApplication;
   let prisma: PrismaService;
+  let jwt: JwtService;
+
+  let user: any;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -17,15 +22,31 @@ describe('Create user controller', () => {
     app = moduleFixture.createNestApplication();
     app.useGlobalPipes(new ValidationPipe());
     prisma = moduleFixture.get(PrismaService);
+    jwt = moduleFixture.get(JwtService);
 
     await app.init();
+
+    user = {
+      id: randomUUID(),
+      name: 'admin',
+      email: 'admin@email.com',
+    };
+
+    await prisma.user.create({
+      data: user,
+    });
   });
 
   test('/users (POST)', async () => {
-    const response = await request(app.getHttpServer()).post('/users').send({
-      name: 'Gabriel Teodoro',
-      email: 'gabriel@email.com',
-    });
+    const token = jwt.sign(user);
+
+    const response = await request(app.getHttpServer())
+      .post('/users')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        name: 'Gabriel Teodoro',
+        email: 'gabriel@email.com',
+      });
 
     expect(response.statusCode).toBe(201);
   });
